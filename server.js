57d784faa946a075c803fd7f201f19a75e4fb8d1
Shane Dupree <shane.dupree@gmail.com>
2021-12-15 19:07:55 -0500

    Add OAuth authentication.

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const methodOverride = require('method-override');
+
+const isLoggedIn = require('./config/auth');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 // Load configuration from file.
 require('dotenv').config();
@@ -11,8 +18,8 @@ require('dotenv').config();
 // Connect to the MongoDB database with Mongoose.
 require('./config/database');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// Require Passport module for OAuth.
+require('./config/passport');
 
 const app = express();
 
@@ -26,6 +33,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_m'));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
