9fde55b2e3063603aaaa7e6be37e1b19de8932ee
Shane Dupree <shane.dupree@gmail.com>
2021-12-20 19:23:04 -0500

    Refactor to use async/await in some functions.

--- controllers/houses.js
+++ controllers/houses.js
@@ -12,94 +12,74 @@ module.exports = {
 };
 
 async function index(req, res) {
-  const houses = await House.find({});
-  // Check for errors here, some fine day... (check houses.length)
-  console.log(houses.length);
+  const houses = await House.find({}).sort({createdAt: 'asc'});  // I assume "sort({createdAt: 'asc'})" is actually the default, but let's be explicit.
   res.render('houses/index', {title: "All Houses", houses});
 }
 
-function show(req, res) {
-  House.findById(req.params.id).populate('pictures').exec(function (err, house) {
-    res.render("houses/show", { title: "House Detail", house });
-  });
+async function show(req, res) {
+  const house = await House.findById(req.params.id).populate('pictures').exec();
+  res.render("houses/show", { title: "House Detail", house });
 }
 
 function newHouse(req, res) {
   res.render("houses/new", { title: "Add House" });
 }
 
-function create(req, res) {
-  // delete any empty properties from req.body
-  for (let key in req.body) {
-    if (req.body[key] === "") delete req.body[key];
-  }
-  
-  // Add User ID into new House object.
-  req.body.user = req.user._id;
+async function create(req, res, next) {
+  try {
+    // Delete empty properties from req.body.
+    for (let key in req.body) { if (req.body[key] === "") delete req.body[key]; }
+    
+    // Make new House object and save it.
+    req.body.user = req.user._id;
+    const house = new House(req.body);
+    await house.save();
 
-  const house = new House(req.body);
-  house.save(function (err) {
-    // Rudimentary error handling.
-    if (err) {
-      console.log(err);
-      return res.redirect("/houses/new");
-    }
     // Save default picture as first 'Picture'.
     const picture = new Picture({
       URI: req.body.featuredPicture,
       house: house._id,
-      user: res.locals.user._id
+      user: req.user._id
     });
-    picture.save(function(err) {
-      if (err) {
-        console.log(err);
-        return res.redirect("/houses/new");
-      }
+    await picture.save();
 
-      // Add new Picture back to House and save it.
-      house.pictures.push(picture._id);
-      house.save(function(err) {
-        if (err) {
-          console.log(err);
-          return res.redirect("/houses/new");
-        }
+    // Add new Picture back to House and save it.
+    house.pictures.push(picture._id);
+    await house.save();
 
-        res.redirect(`/houses/${house._id}`);
-      });
-    });
-  });
+    res.redirect(`/houses/${house._id}`);
+  } catch(e) {
+    console.log(e);
+    // res.redirect('/houses/new');
+  }
 }
 
 function edit(req, res) {
   House.findById(req.params.id).populate('pictures').exec(function (err, house) {
-    res.render("houses/edit", { title: "Edit House", house });
+    res.render('houses/edit', { title: 'Edit House', house });
   });
 }
 
 async function update(req, res) {
-  const house = await House.findById(req.params.id);
-  
-  // Delete any empty properties from req.bodye
-  for (const key in req.body) {
-    if (req.body[key] === '') delete req.body[key];
-  }
-  
-  // Update (updatable) house keys from req.body.
-  for(const key of ['name', 'location', 'latLon', 'constructionYear', 'archStyle', 'featuredPicture', 'description']) {
-    house[key] = req.body[key];
-  }
-
-  // Do picture management here?
-
-  // Save house and render.
-  house.save(function(err) {
-    if (err) {
-      console.log(err);
-      return res.redirect(`/houses/${house._id}`);
+  try {
+    const house = await House.findById(req.params.id);
+    
+    // Delete any empty properties from req.body.
+    for (const key in req.body) { if (req.body[key] === '') delete req.body[key]; }
+    
+    // Update (updatable) house keys from req.body.
+    for(const key of ['name', 'location', 'latLon', 'constructionYear', 'archStyle', 'featuredPicture', 'description']) {
+      house[key] = req.body[key];
     }
 
+    // Save house.
+    await house.save();
+
     res.redirect(`/houses/${house._id}`);
-  });  
+  } catch(e) {
+    console.log(e);
+    res.redirect('/houses/new');
+  }
 }
 
 function deleteOne(req, res) {
