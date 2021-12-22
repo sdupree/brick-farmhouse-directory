6fcc2d4d4987c2083e7d9d9fabe560b4a66fa5a6
Shane Dupree <shane.dupree@gmail.com>
2021-12-22 13:44:52 -0500

    Update tab styling and house edit page styling.

--- public/javascripts/picture_changer.js
+++ public/javascripts/picture_changer.js
@@ -1,8 +1,8 @@
 const imgEl = document.getElementById('house-picture-img');
 let i = 0;
 
-document.getElementById('nav-picture-prev').addEventListener('click', prevPicture);
-document.getElementById('nav-picture-next').addEventListener('click', nextPicture);
+if(document.getElementById('nav-picture-prev')) document.getElementById('nav-picture-prev').addEventListener('click', prevPicture);
+if(document.getElementById('nav-picture-next')) document.getElementById('nav-picture-next').addEventListener('click', nextPicture);
 
 function nextPicture() {
   i++;
