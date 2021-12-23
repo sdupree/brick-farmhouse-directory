13cf72b7964c4a20770644df70c451272da43f7d
Shane Dupree <shane.dupree@gmail.com>
2021-12-22 19:37:54 -0500

    Automatically set map boundaries.

--- controllers/map.js
+++ controllers/map.js
@@ -5,5 +5,25 @@ module.exports = { map };
 async function map(req, res) {
   const houses = await House.find({});
   res.locals.page = 'map';
-  res.render('map/map', { title: 'House Map', houses });
+
+  const lats = [];
+  const lons = [];
+  for(house of houses) {
+    const latLon = house.latLon.split(',');
+    lats.push(parseFloat(latLon[0]));
+    lons.push(parseFloat(latLon[1]));
+  }
+  lats.sort();
+  lons.sort();
+  const bounds = {
+    north: lats[lats.length - 1],
+    south: lats[0],
+    east: lons[0],
+    west: lons[lons.length - 1]
+  }
+
+  // console.log(lats);
+  // console.log(lons);
+
+  res.render('map/map', { title: 'House Map', houses, bounds });
 }
